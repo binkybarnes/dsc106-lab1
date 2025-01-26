a65ce28f2a7675aa72d32975fcf4fc1a52c3501b
binkybarnes <cottonjohn2000@gmail.com>
2025-01-25 21:36:26 -0800

    finished lab3

---
+++ global.js
@@ -0,0 +1,89 @@
+// console.log("IT’S ALIVE!");
+
+// function $$(selector, context = document) {
+//   return Array.from(context.querySelectorAll(selector));
+// }
+
+// let navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
+// currentLink?.classList.add("current");
+// console.log(currentLink.classList);
+
+// nav bar
+
+const pages = [
+  { url: "/", title: "Home" },
+  { url: "/projects/", title: "Projects" },
+  { url: "/resume/", title: "Resume" },
+  { url: "/contact/", title: "Contact" },
+  { url: "https://github.com/binkybarnes/", title: "Github" },
+];
+
+const createNavLink = ({ url, title }) => {
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+  const isCurrentPage =
+    a.host === location.host && a.pathname === location.pathname;
+  a.classList.toggle("current", isCurrentPage);
+  if (a.host !== location.host) a.target = "_blank";
+
+  return a;
+};
+
+const nav = document.createElement("nav");
+document.body.prepend(nav);
+pages.forEach((page) => {
+  const link = createNavLink(page);
+  nav.append(link);
+});
+
+// color scheme switcher
+
+const isDark = matchMedia("(prefers-color-scheme: dark)").matches;
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+	<label class="color-scheme">
+   Theme:
+   <select>
+     <option value="light dark">Automatic (${
+       isDark ? "Dark" : "Light"
+     })</option>
+     <option value="light">Light</option>
+     <option value="dark">Dark</option>
+   </select>
+ </label>`
+);
+
+const setColorScheme = (colorScheme) => {
+  console.log("color scheme changed to", colorScheme);
+  document.documentElement.style.setProperty("color-scheme", colorScheme);
+  localStorage.colorScheme = colorScheme;
+};
+const select = document.querySelector("label.color-scheme select");
+if ("colorScheme" in localStorage) {
+  const savedColorSchemeValue = localStorage.colorScheme;
+  setColorScheme(savedColorSchemeValue);
+}
+select.addEventListener("input", function (event) {
+  const colorScheme = event.target.value;
+  setColorScheme(colorScheme);
+});
+
+// contact form space fixer
+const contactForm = document.querySelector("form");
+contactForm?.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const data = new FormData(contactForm);
+  // mailto:leaverou@mit.edu?subject=Hello&body=Sup?
+  let url = `${contactForm.action}?`;
+  for (let [name, value] of data) {
+    console.log(name, encodeURIComponent(value));
+    url += `${name}=${value}&`;
+  }
+  location.href = url;
+});
